598935d6bfc328ae75df4066f676f37371c1f75f
Michael Scholten <michael-scholten@hotmail.nl>
2023-01-01 12:19:16 +0100

    A minimal rust kernel

--- build.sh
+++
@@ -1,8 +0,0 @@
-# compiling on linux
-cargo rustc -- -C link-arg=-nostartfiles
-
-# compiling on macOS
-#cargo rustc -- -C link-args="-e __start -static -nostartfiles"
-
-# compiling on Windows (change the file extension)
-#cargo rustc -- -C link-args="/ENTRY:_start /SUBSYSTEM:console"
